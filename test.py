26a85d7eb18166b3a761797f191a44013077fe17
Ludvig Karlsson <ludvig@Ludvigs-MBP.lan>
2020-04-14 12:12:58 +0200

    Zip file works

---
+++ test.py
@@ -0,0 +1,17 @@
+import zipfile
+
+zf = zipfile.ZipFile('STYLE.zip', 'w')
+
+try: 
+    print("Adding style_transfer.py")
+    zf.write('style_transfer.py')
+finally:
+    print("Closing")
+    zf.close()
+
+print("STYLE.zip is valid zip file: " + zipfile.is_zipfile('STYLE.zip'))
+
+
+
+
+
