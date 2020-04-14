d51e23f8ef545a9f12d4b0feb1f041bc517f1bf5
Ludvig Karlsson <ludvig@Ludvigs-MBP.lan>
2020-04-14 14:56:06 +0200

    Zip fix

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
+# Create model zip file
+import zipfile
 
-## Create model zip file
-# import zipfile
+zf = zipfile.ZipFile('STYLE.zip', 'w')
 
-# zf = zipfile.ZipFile('STYLE.zip', 'w')
+try: 
+    print("Adding style_transfer.py")
+    zf.write('style_transfer.py')
+finally:
+    print("Closing")
+    zf.close()
 
-# try: 
-#     print("Adding style_transfer.py")
-#     zf.write('style_transfer.py')
-# finally:
-#     print("Closing")
-#     zf.close()
-
-# print("STYLE.zip is valid zip file: " + zipfile.is_zipfile('STYLE.zip'))
+print("STYLE.zip is valid zip file: " + str(zipfile.is_zipfile('STYLE.zip')))
 
 
 
